74ee5152494ff4df02bcca8c301ca3b04f500c1d
james.jeon <no@email.com>
2021-05-14 20:32:27 -0500

    parse time to utc

--- config.py
+++ config.py
@@ -2,3 +2,4 @@ import os
 
 PRODUCTION = os.getenv("PRODUCTION", default=False)
 SECRET = os.getenv("SECRET", default=None)
+STORE_PATH = os.getenv("STORE_PATH", default=".")
--- controller/Photo.py
+++ controller/Photo.py
@@ -1,7 +1,13 @@
 import io
+import os
+from datetime import datetime
 from uuid import uuid4
 
+import config
+import pytz
 from PIL import ExifTags, Image
+from schema.Photos import Photos
+from timezonefinder import TimezoneFinder
 
 
 class Photo(object):
@@ -22,16 +28,16 @@ class Photo(object):
         if file_format.lower() not in supported_format:
             raise ValueError(f"Image format of {file_format} is not supported")
 
-
         # read image
         file_bytes = await file.read()
         img = Image.open(io.BytesIO(file_bytes))
         img_exif = img._getexif()
 
         exif = {}
-        for key, val in img_exif.items():
-            if key in ExifTags.TAGS:
-                exif[ExifTags.TAGS[key]] = val
+        if img_exif is not None:
+            for key, val in img_exif.items():
+                if key in ExifTags.TAGS:
+                    exif[ExifTags.TAGS[key]] = val
         width, height = img.size
         id = str(uuid4())
         payload = {
@@ -42,8 +48,7 @@ class Photo(object):
             "original_model": exif["Model"] if "Model" in exif else None,
             "original_width": width,
             "original_height": height,
-            "new_filename": f"{id}.png",
-            "thumbnail": f"{id}-thumbnail.png",
+            "new_filename": f"{id}.{file_format}",
             "owner" : owner_id,
             "status": 1
         }
@@ -52,15 +57,33 @@ class Photo(object):
             payload["latitude"] = float((exif["GPSInfo"][2][0] + exif["GPSInfo"][2][1] / 60 + exif["GPSInfo"][2][2] / 3600) * 1 if exif["GPSInfo"][1] == "N" else -1)
             payload["longitude"] = float((exif["GPSInfo"][4][0] + exif["GPSInfo"][4][1] / 60 + exif["GPSInfo"][4][2] / 3600) * (1 if exif["GPSInfo"][3] == "E" else -1))
 
-        print(payload)
-        with open(f"{id}.{file_format}",'wb+') as f:
+            if payload["original_datetime"] is not None:
+                tf = TimezoneFinder()
+                timezone_str = tf.certain_timezone_at(lat=payload["latitude"], lng=payload["longitude"])
+                local = pytz.timezone(timezone_str)
+                naive = datetime.strptime(payload["original_datetime"], "%Y:%m:%d %H:%M:%S")      
+                local_dt = local.localize(naive, is_dst=None)
+                utc_dt = local_dt.astimezone(pytz.utc)  
+                payload["original_datetime"] = datetime.strptime(utc_dt.strftime("%Y:%m:%d %H:%M:%S"), "%Y:%m:%d %H:%M:%S")
+        else:
+            if payload["original_datetime"] is None:
+                payload["original_datetime"] = datetime.utcnow()
+            else:
+                payload["original_datetime"] = datetime.strptime(payload["original_datetime"], "%Y:%m:%d %H:%M:%S")
+
+        complete_path = f"{config.STORE_PATH}/{owner_id}/"
+        if not os.path.exists(complete_path):
+            os.makedirs(complete_path)
+
+        with open(f"{complete_path}{id}.{file_format}",'wb+') as f:
             f.write(file_bytes)
             f.close()
         img.thumbnail((2560,2560), Image.LANCZOS)
-        img.save(f"{id}-resize.{file_format}", file_format)
+        img.save(f"{complete_path}{id}-resize.{file_format}", file_format)
         img.thumbnail((512,512), Image.LANCZOS)
-        img.save(f"{id}-thumbnail.{file_format}", file_format)
-
+        img.save(f"{complete_path}{id}-thumbnail.{file_format}", file_format)
+        print(payload)
+        await Photos.insert(**payload)
         return payload
 
     async def __process_video(self, file, owner_id):
