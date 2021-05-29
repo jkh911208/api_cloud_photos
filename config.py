9c5866650ccecf4b331b9136c4e0086a846473e4
james.jeon <no@email.com>
2021-05-29 00:04:24 -0500

    remove default for secret

--- config.py
+++ config.py
@@ -3,4 +3,4 @@ import os
 PRODUCTION = os.getenv("PRODUCTION").lower() in {'true', '1'}
 SECRET = os.getenv("SECRET", default="secret")
 STORE_PATH = os.getenv("STORE_PATH", default=".")
-APP_SECRET = os.getenv("APP_SECRET", default="ihavenoidea")
+APP_SECRET = os.getenv("APP_SECRET")
